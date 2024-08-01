62d445504c95932a7a3738f84785d5adef6609eb
josephgress@prestigesports.app <josephgress@prestigesports.app>
2024-07-31 21:29:30 -0500

    static

--- portfolio-frontend/next.config.mjs
+++ portfolio-frontend/next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
+/*
+// Prev
+/** @type {import('next').NextConfig} 
 const nextConfig = {};
 
 export default nextConfig;
+*/
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/portfolio-frontend",
+    output: "export",  // Enables static exports
+    reactStrictMode: true,
+  };
+  
+  export default nextConfig;
